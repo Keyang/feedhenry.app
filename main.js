ff4af262707f17b2e8f34470bec8c4b5dee9688c
KeyangXiang <keyang.xiang@gmail.com>
2015-07-13 20:18:25 +0100

    clean

--- main.js
+++ main.js
@@ -1,8 +1,12 @@
 var app=require("app");
+var globalShortcut = require('global-shortcut');
 app.on("window-all-closed",function(){
   app.quit();
 })
 app.on("ready",function(){
+  var ret = globalShortcut.register('ctrl+alt+d', function() {
+    mWindow.getWindow().openDevTools();
+  });
   var mWindow=require("./mainWindow");
   mWindow.open("index.html",{width:1400,height:800});
 })
